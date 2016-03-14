eeaef502fba22b8927e984b8e14fcf5ff6e470bf
Elder Lucas <elderlucas@gmail.com>
2016-03-14 16:19:22 -0300

    HTML File
    Alteração no  nome do arquivo html dentro da função hello( )

--- robo_kpacitor.py
+++ robo_kpacitor.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('kpacitorpi.html')
+    return render_template('robo_kpacitor.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
